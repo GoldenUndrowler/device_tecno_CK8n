6e6ee30d1ec3fee765ff55f7acf4455501511ef9
xoo2001 <ekora2001@gmail.com>
2025-03-17 18:55:32 +0700

    CK8n: Build `vendor_boot` in userdebug

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,8 +19,11 @@ AB_OTA_PARTITIONS += \
     vbmeta \
     vbmeta_system \
     vbmeta_vendor \
-    vendor \
-    vendor_boot
+    vendor
+
+ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+AB_OTA_PARTITIONS += vendor_boot
+endif
 
 # Architecture
 TARGET_ARCH := arm64
